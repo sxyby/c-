b6deaa1b08ad3a5f71b6599b2b57672484962fa3
xby <90120439+sxyby@users.noreply.github.com>
2023-05-18 09:40:59 +0800

    Add files via upload

---
+++ c++第七次作业/单词长度.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class text
+{
+	public:
+	text()
+	{
+		n = 0;
+		for(int i = 0;i<100;i++)
+		{
+			cin>>a[i];
+			n++;
+			if(a[i].back() == ' ')
+			{
+				continue;
+			}
+			if(a[i].back() == '.')
+			{
+				break;
+			}
+		}	
+		show();
+	}
+	void show()
+	{	
+		for(int i = 0;i<n;i++)
+		{
+			if(a[i].back() == '.'||a[i].back() == ' ')
+			{
+				std::cout << a[i].length()-1<<" ";
+			}
+			else
+			std::cout << a[i].length()<<" ";
+		}	
+	}
+	private:
+		string a[100];
+		int n;
+};
+int main()
+{
+	text a;
+	return 0;
+}
